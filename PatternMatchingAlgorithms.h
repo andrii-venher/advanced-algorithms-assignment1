c754d9af0b1ec974f849967ff5406bbc74cf05a1
Andrii Venher <vengerandr@gmail.com>
2023-04-15 16:24:58 +0200

    Add Reverse Sunday pattern matching algorithm

--- PatternMatchingAlgorithms.h
+++ PatternMatchingAlgorithms.h
@@ -326,6 +326,51 @@ public:
     std::string get_name() override { return "Sunday"; }
 };
 
+class ReverseSundayPatternMatchingAlgorithm : public PatternMatchingAlgorithm {
+public:
+    int find_matches(const std::string &t, const std::string &p) override {
+        size_t t_size = t.size();
+        size_t p_size = p.size();
+
+        std::vector<int> positions(256, -1);
+
+        for (int i = 0; i < p_size; ++i) {
+            positions[p[i]] = i;
+        }
+        int matches = 0;
+
+        size_t t_index = 0;
+        while (t_index <= t_size - p_size) {
+            int p_index = 0;
+
+            while (p_index < p_size && p[p_index] == t[t_index + p_index]) {
+                p_index++;
+            }
+
+            if (p_index >= p_size) {
+                matches++;
+
+                if (t_index + p_size < t_size) {
+                    t_index += p_size - positions[t[t_index + p_size]];
+                } else {
+                    ++t_index;
+                }
+            } else {
+                int shift = p_index - positions[t[t_index + p_index]];
+
+                if (shift > 0) {
+                    t_index += shift;
+                } else {
+                    ++t_index;
+                }
+            }
+        }
+        return matches;
+    }
+
+    std::string get_name() override { return "Reverse Sunday"; }
+};
+
 class RabinKarpPatternMatchingAlgorithm : public PatternMatchingAlgorithm {
 public:
     int find_matches(const std::string &t, const std::string &p) override {
--- main.cpp
+++ main.cpp
@@ -4,24 +4,19 @@
 int main() {
     TestRunner test_runner;
 
-//    std::vector<PatternMatchingAlgorithm *> algorithms = {
-//            new BruteForcePatternMatchingAlgorithm(),
-//            new BinarySundayPatternMatchingAlgorithm(),
-//            new SundayPatternMatchingAlgorithm(),
-//            new RabinKarpPatternMatchingAlgorithm(),
-//            new GusfieldZPatternMatchingAlgorithm(),
-//            new KMPPatternMatchingAlgorithm(),
-//            new FSMPatternMatchingAlgorithm(),
-//    };
+    std::vector<PatternMatchingAlgorithm *> algorithms = {
+            new SundayPatternMatchingAlgorithm(),
+            new ReverseSundayPatternMatchingAlgorithm(),
+    };
+
+    test_runner.test_algorithms(algorithms, test_runner.text, test_runner.small_pattern);
+
+//    std::vector<WildcardPatternMatchingAlgorithm *> wildcard_algorithms =
+//            {
+//                    new WildcardBruteForcePatternMatchingAlgorithm(),
+//            };
 //
-//    test_runner.test_algorithms(algorithms, test_runner.text, test_runner.large_pattern);
-
-    std::vector<WildcardPatternMatchingAlgorithm *> wildcard_algorithms =
-            {
-                    new WildcardBruteForcePatternMatchingAlgorithm(),
-            };
-
-    test_runner.test_wildcards_algorithms_using_test_cases(wildcard_algorithms);
+//    test_runner.test_wildcards_algorithms_using_test_cases(wildcard_algorithms);
 
 //    test_runner.test_2d_rabin_karp(4);
 }
