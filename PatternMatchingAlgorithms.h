dd7f128d36a081405f7d1ea2344d292d53040cf7
Andrii Venher <vengerandr@gmail.com>
2023-04-15 17:59:48 +0200

    Fix wildcard pattern matching algorithms

--- PatternMatchingAlgorithms.h
+++ PatternMatchingAlgorithms.h
@@ -17,8 +17,139 @@ public:
 };
 
 class WildcardPatternMatchingAlgorithm : public Algorithm {
+private:
+    int find_next_star_index(const std::string &p, int start_index) {
+        for (int i = start_index; i < p.size(); ++i) {
+            if (p[i] == '\\') {
+                i++;
+            } else if (p[i] == '*') {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+protected:
+    struct Match {
+        bool found;
+        int start_index;
+        int end_index;
+
+        Match(bool found, int start_index, int end_index) : found(found), start_index(start_index), end_index(end_index) {}
+    };
+
+    virtual Match find_match_without_star(const std::string &t, const std::string &p) = 0;
+
 public:
-    virtual bool find_match(const std::string &t, const std::string &p) = 0;
+
+    bool find_match(const std::string &t, const std::string &pattern) {
+        std::string p = pattern;
+
+        size_t t_size = t.size();
+        size_t p_size = p.size();
+
+        // Check if pattern has correct format, e.g. after backslash is a wildcard or another backslash
+        if (p_size == 1 && p[0] == '\\') {
+            throw std::logic_error("Pattern is incorrect. Invalid sequence at index 0: \\");
+        }
+        for (int i = 0; i < p_size; ++i) {
+            if (p[i] == '\\') {
+                if (i < p_size - 1) {
+                    if (p[i + 1] == '*' || p[i + 1] == '?' || p[i + 1] == '\\') {
+                        i++;
+                    } else {
+                        throw std::logic_error("Pattern is incorrect. Invalid sequence at index " + std::to_string(i) + ": "
+                                               + std::string(1, p[i]) + std::string(1, p[i + 1]));
+                    }
+                }
+                if (i == p_size - 1 && p_size >= 1 && p[i - 1] != '\\') {
+                    throw std::logic_error(
+                            "Pattern is incorrect. Invalid sequence at index " + std::to_string(i) + ": \\");
+                }
+            }
+        }
+
+        // Preprocess pattern to merge duplicate stars which are next to each other
+        std::stringstream new_p;
+        for (int i = 0; i < p_size; ++i) {
+            if (p[i] == '*') {
+                int j = i + 1;
+                while (j < p_size && p[j] == '*') {
+                    j++;
+                }
+                i = j - 1;
+            }
+            new_p << p[i];
+        }
+        p = new_p.str();
+        // Trim star from start
+        if (p[0] == '*') {
+            p = p.substr(1);
+        }
+        // Trim star from end if not escaped
+        if (p.size() > 1 && p[p.size() - 1] == '*' && p[p.size() - 2] != '\\') {
+            p = p.substr(0, p.size() - 1);
+        }
+        p_size = p.size();
+
+        // If pattern is empty, then trivial
+        if (p_size == 0) {
+            return true;
+        }
+
+        int text_cursor = 0;
+        int pattern_cursor = 0;
+
+        int index_of_first_star = find_next_star_index(p, 0);
+        if (index_of_first_star >= 0) {
+            Match initial_match = find_match_without_star(t, p.substr(0, index_of_first_star));
+            if (!initial_match.found) {
+                return false;
+            }
+            text_cursor = initial_match.end_index;
+            pattern_cursor = index_of_first_star;
+        } else {
+            return find_match_without_star(t, p).found;
+        }
+
+        while (pattern_cursor < p_size) {
+            // End of the text reached, pattern not found yet
+            if (text_cursor >= t_size) {
+                break;
+            }
+
+            int index_of_next_star = find_next_star_index(p, pattern_cursor + 1);
+            // This is the last star in the pattern
+            if (index_of_next_star == -1) {
+                // No more pattern characters after it, so trivial
+                if (pattern_cursor == p_size - 1) {
+                    return true;
+                }
+                    // Check if there is a match in the rest of the pattern
+                else {
+                    return find_match_without_star(t.substr(text_cursor), p.substr(pattern_cursor + 1)).found;
+                }
+            }
+                // There is another star in the pattern
+            else {
+                std::string text_to_match = t.substr(text_cursor);
+                std::string pattern_to_match = p.substr(pattern_cursor + 1, index_of_next_star - pattern_cursor - 1);
+                Match match_between = find_match_without_star(text_to_match, pattern_to_match);
+                if (!match_between.found) {
+                    return false;
+                }
+                pattern_cursor = index_of_next_star;
+                text_cursor += match_between.end_index;
+            }
+
+            // Last pattern star reached, and it is at the last index, so trivial
+            if (pattern_cursor == p_size - 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 };
 
 class BruteForcePatternMatchingAlgorithm : public PatternMatchingAlgorithm {
@@ -53,14 +184,7 @@ public:
 
 class WildcardBruteForcePatternMatchingAlgorithm : public WildcardPatternMatchingAlgorithm {
 private:
-    struct Match {
-        bool found;
-        int start_index;
-        int end_index;
-        Match(bool found, int start_index, int end_index) : found(found), start_index(start_index), end_index(end_index) {}
-    };
-
-    Match find_match_without_star(const std::string &t, const std::string &p) {
+    Match find_match_without_star(const std::string &t, const std::string &p) override {
         size_t t_size = t.size();
         size_t p_size = p.size();
 
@@ -90,18 +214,17 @@ private:
                     pattern_cursor++;
                 }
             }
-            // Question mark wildcard, match any character
+                // Question mark wildcard, match any character
             else if (current_pattern_char == '?') {
                 consecutive_matches++;
-             }
-            // Any other character, match exactly
+            }
+                // Any other character, match exactly
             else if (current_text_char != current_pattern_char) {
                 pattern_cursor = 0;
                 text_cursor -= consecutive_matches - 1;
                 consecutive_matches = 0;
                 continue;
-            }
-            else {
+            } else {
                 consecutive_matches++;
             }
 
@@ -112,133 +235,12 @@ private:
         bool found = pattern_cursor == p_size;
         if (found) {
             return {true, text_cursor - consecutive_matches, text_cursor};
-        }
-        else {
+        } else {
             return {false, -1, -1};
         }
     }
 
 public:
-    bool find_match(const std::string &t, const std::string &pattern) override {
-        std::string p = pattern;
-
-        size_t t_size = t.size();
-        size_t p_size = p.size();
-
-        // Check if pattern has correct format, i.e. after backslash is not a wildcard and not another backslash
-        if (p_size == 1 && p[0] == '\\') {
-            throw std::logic_error("Pattern is incorrect. Invalid sequence at index 0: \\");
-        }
-        for (int i = 0; i < p_size; ++i) {
-            if (p[i] == '\\') {
-                if (i < p_size - 1 && p[i + 1] != '*' && p[i + 1] != '?' && p[i + 1] != '\\') {
-                    throw std::logic_error("Pattern is incorrect. Invalid sequence at index " + std::to_string(i) + ": "
-                                           + std::string(1, p[i]) + std::string(1, p[i + 1]));
-                }
-                if (i == p_size - 1 && p_size >= 1 && p[i - 1] != '\\') {
-                    throw std::logic_error(
-                            "Pattern is incorrect. Invalid sequence at index " + std::to_string(i) + ": \\");
-                }
-            }
-        }
-
-        // Preprocess pattern to merge duplicate stars which are next to each other
-        std::stringstream new_p;
-        for (int i = 0; i < p_size; ++i) {
-            if (p[i] == '*') {
-                int j = i + 1;
-                while (j < p_size && p[j] == '*')
-                {
-                    j++;
-                }
-                i = j - 1;
-            }
-            new_p << p[i];
-        }
-        p = new_p.str();
-        // Trim star from start
-        if (p[0] == '*') {
-            p = p.substr(1);
-        }
-        // Trim star from end if not escaped
-        if (p.size() > 1 && p[p.size() - 1] == '*' && p[p.size() - 2] != '\\') {
-            p = p.substr(0, p.size() - 1);
-        }
-        p_size = p.size();
-
-        // If pattern is empty, then trivial
-        if (p_size == 0) {
-            return true;
-        }
-
-        int text_cursor = 0;
-        int pattern_cursor = 0;
-
-        int index_of_first_star = -1;
-        for (int i = 0; i < p_size; ++i) {
-            if (p[i] == '*' && (i == 0 || (i > 0 && p[i - 1] != '\\') || (i > 1 && p[i - 1] == '\\' && p[i - 2] == '\\'))) {
-                index_of_first_star = i;
-                break;
-            }
-        }
-        if (index_of_first_star >= 0) {
-            Match initial_match = find_match_without_star(t, p.substr(0, index_of_first_star));
-            if(!initial_match.found)
-            {
-                return false;
-            }
-            text_cursor = initial_match.end_index;
-            pattern_cursor = index_of_first_star;
-        }
-        else {
-            return find_match_without_star(t, p).found;
-        }
-
-        while (pattern_cursor < p_size) {
-            // End of the text reached, pattern not found yet
-            if (text_cursor >= t_size) {
-                break;
-            }
-
-            int index_of_next_star = -1;
-            for (int i = pattern_cursor + 1; i < p_size; ++i) {
-                if (p[i] == '*' && p[i - 1] != '\\') {
-                    index_of_next_star = i;
-                    break;
-                }
-            }
-            // This is the last star in the pattern
-            if (index_of_next_star == -1) {
-                // No more pattern characters after it, so trivial
-                if (pattern_cursor == p_size - 1) {
-                    return true;
-                }
-                    // Check if there is a match in the rest of the pattern
-                else {
-                    return find_match_without_star(t.substr(text_cursor), p.substr(pattern_cursor + 1)).found;
-                }
-            }
-                // There is another star in the pattern
-            else {
-                std::string text_to_match = t.substr(text_cursor);
-                std::string pattern_to_match = p.substr(pattern_cursor + 1, index_of_next_star - pattern_cursor - 1);
-                Match match_between = find_match_without_star(text_to_match, pattern_to_match);
-                if (!match_between.found) {
-                    return false;
-                }
-                pattern_cursor = index_of_next_star;
-                text_cursor += match_between.end_index;
-            }
-
-            // Last pattern star reached, and it is at the last index, so trivial
-            if (pattern_cursor == p_size - 1) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
     std::string get_name() override {
         return "Wildcard brute force";
     }
@@ -326,35 +328,61 @@ public:
     std::string get_name() override { return "Sunday"; }
 };
 
-class ReverseSundayPatternMatchingAlgorithm : public PatternMatchingAlgorithm {
-public:
-    int find_matches(const std::string &t, const std::string &p) override {
-        size_t t_size = t.size();
-        size_t p_size = p.size();
+class WildcardSundayPatternMatchingAlgorithm : public WildcardPatternMatchingAlgorithm {
+private:
+    Match find_match_without_star(const std::string &t, const std::string &p) override {
+        int t_size = t.size();
+        int p_size = p.size();
 
-        std::vector<int> positions(256, -1);
+        int last_wildcard_question_mark_index = -1;
+        for (int i = p_size - 1; i >= 0; --i) {
+            if (p[i] == '?' && (i == 0 || p[i - 1] != '\\')) {
+                last_wildcard_question_mark_index = i;
+                break;
+            }
+        }
+
+        std::vector<int> positions(256, last_wildcard_question_mark_index);
+
+        int chars_escaped = 0;
 
         for (int i = 0; i < p_size; ++i) {
+            if (p[i] == '\\' && i < p_size - 1) {
+                i++;
+                chars_escaped++;
+            }
             positions[p[i]] = i;
         }
-        int matches = 0;
 
-        size_t t_index = 0;
-        while (t_index <= t_size - p_size) {
+        int t_index = 0;
+        while (t_index <= t_size - p_size + chars_escaped) {
             int p_index = 0;
 
-            while (p_index < p_size && p[p_index] == t[t_index + p_index]) {
-                p_index++;
+            int delta = 0;
+
+            while (p_index < p_size) {
+                char current_text_char = t[t_index + p_index - delta];
+                char current_pattern_char = p[p_index];
+                bool escaped = false;
+
+                if (p[p_index] == '\\') {
+                    delta++;
+                    p_index++;
+                    current_pattern_char = p[p_index];
+                    escaped = true;
+                }
+
+                if (current_text_char == current_pattern_char ||
+                    !escaped && current_pattern_char == '?') {
+                    p_index++;
+                    continue;
+                }
+
+                break;
             }
 
             if (p_index >= p_size) {
-                matches++;
-
-                if (t_index + p_size < t_size) {
-                    t_index += p_size - positions[t[t_index + p_size]];
-                } else {
-                    ++t_index;
-                }
+                return {true, t_index, t_index + p_size - chars_escaped};
             } else {
                 int shift = p_index - positions[t[t_index + p_index]];
 
@@ -365,10 +393,12 @@ public:
                 }
             }
         }
-        return matches;
+
+        return {false, -1, -1};
     }
 
-    std::string get_name() override { return "Reverse Sunday"; }
+public:
+    std::string get_name() override { return "Wildcard Sunday"; }
 };
 
 class RabinKarpPatternMatchingAlgorithm : public PatternMatchingAlgorithm {
--- main.cpp
+++ main.cpp
@@ -4,19 +4,19 @@
 int main() {
     TestRunner test_runner;
 
-    std::vector<PatternMatchingAlgorithm *> algorithms = {
-            new SundayPatternMatchingAlgorithm(),
-            new ReverseSundayPatternMatchingAlgorithm(),
-    };
-
-    test_runner.test_algorithms(algorithms, test_runner.text, test_runner.small_pattern);
-
-//    std::vector<WildcardPatternMatchingAlgorithm *> wildcard_algorithms =
-//            {
-//                    new WildcardBruteForcePatternMatchingAlgorithm(),
-//            };
+//    std::vector<PatternMatchingAlgorithm *> algorithms = {
+//            new SundayPatternMatchingAlgorithm(),
+//    };
 //
-//    test_runner.test_wildcards_algorithms_using_test_cases(wildcard_algorithms);
+//    test_runner.test_algorithms(algorithms, test_runner.text, test_runner.small_pattern);
+
+    std::vector<WildcardPatternMatchingAlgorithm *> wildcard_algorithms =
+            {
+                    new WildcardBruteForcePatternMatchingAlgorithm(),
+                    new WildcardSundayPatternMatchingAlgorithm(),
+            };
+
+    test_runner.test_wildcards_algorithms_using_test_cases(wildcard_algorithms);
 
 //    test_runner.test_2d_rabin_karp(4);
 }
